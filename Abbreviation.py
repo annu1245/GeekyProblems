4506d5e90a95bd9479c94ae765fb7c005ef2d283
annu1245 <annupoddar444@gmail.com>
2019-12-08 11:53:36 +0530

    Abbreviation

---
+++ Abbreviation.py
@@ -0,0 +1,14 @@
+# Abbreviation
+
+x = 'aBcdhf'
+y = 'ABCDL'
+x = x.upper()
+i = 0
+while j<len(x):
+    if x[i] not in y:
+        x = x.strip(x[i])
+    i+=1
+if x == y:
+    print("yes")
+else:
+    print("no")
